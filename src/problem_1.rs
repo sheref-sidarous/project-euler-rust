946e18cfe20df53d5db8d15a2439e87e52798efc
Sheref Sidarous <sherefyounan@gmail.com>
2022-10-05 22:24:03 -0700

    some rearrangment

---
+++ src/problem_1.rs
@@ -0,0 +1,10 @@
+
+pub fn solution() {
+    let mut sum = 0;
+    for i in 0..1000 {        
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;            
+        }        
+    }
+    println!("result = {}", sum);
+}

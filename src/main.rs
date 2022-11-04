57065aeae898177d9756142c80204b97bf5b1f1e
Sheref Sidarous <sherefyounan@gmail.com>
2022-11-03 19:20:15 -0700

    doing problem 15 as async, was trying to use all cores .. WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(future_join)]
+
 mod bigint;
 mod indexed_iter;
 
@@ -6,6 +8,21 @@ mod indexed_iter;
 //mod problem_14;
 mod problem_15;
 
+use tokio;
+use std::future::join;
+
 fn main() {
-    problem_15::solution();
+    
+    let mut rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(4)
+        .build()
+        .unwrap();
+
+    //let task1 = rt.spawn(problem_15::solution());
+    //let task2 = rt.spawn(problem_15::solution());
+
+    rt.block_on( async {
+        //join!(task1, task2).await;
+        join!(problem_15::solution(), problem_15::solution()).await;
+    } );
 }
--- src/problem_15.rs
+++ src/problem_15.rs
@@ -1,8 +1,12 @@
+use std::future::Future;
+use std::future::join;
+use async_recursion::async_recursion;
 
 static mut no_of_paths : u64 = 0;
 static mut no_of_calls : u64 = 0;
 
-fn visit<const G : u32>(x : u32, y: u32) {
+#[async_recursion]
+async fn visit<const G : u32>(x : u32, y: u32) {
 
     unsafe { no_of_calls += 1 };
 
@@ -10,19 +14,31 @@ fn visit<const G : u32>(x : u32, y: u32) {
         unsafe { no_of_paths += 1 };
     }
 
-    if x < G {
-        visit::<G>(x +1, y);
-    }
+    let x_branch = if x < G {
+        Some(visit::<G>(x +1, y))
+    } else {
+        None
+    };
 
-    if y < G {
-        visit::<G>(x, y+1);
-    }
+    let y_branch = if y < G {
+        Some(visit::<G>(x, y+1))
+    } else {
+        None
+    };
+
+    match (x_branch, y_branch) {
+        (Some(x), Some(y)) => {join!(x,y).await;},
+        (Some(x), None) => x.await,
+        (None, Some(y)) => y.await,
+        (None, None) => {},
+    };
+
 
 }
 
-pub fn solution() {
+pub async fn solution() {
 
-    visit::<20>(0, 0);
+    visit::<20>(0, 0).await;
     unsafe { println!("{}", no_of_paths) };
     unsafe { println!("{}", no_of_calls) };
 

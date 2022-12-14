92e7fa5907faa8dcab8f1667c9ebd4e387fec202
Sheref Sidarous <sherefyounan@gmail.com>
2022-12-13 20:51:02 -0800

    an opt that actually costs more in checks than what it saves

--- src/problem_86.rs
+++ src/problem_86.rs
@@ -1,7 +1,33 @@
 
+static mut is_a_square_number_entry_cnt : u64 = 0;
+static mut is_a_square_number_entry_cnt_opt : u64 = 0;
+
+fn is_a_square_number(input : u64) -> bool {
+
+  unsafe {
+  is_a_square_number_entry_cnt += 1;
+  }
+
+  let last_digit = input / 10;
+
+  match last_digit {
+    2 | 3 | 7 | 8 => {
+      unsafe {is_a_square_number_entry_cnt_opt += 1; }
+      // can't be a square number
+      return false;
+    },
+
+    _ => {}
+  }
+
+  let sq_root = (input as f64).sqrt();
+  return  sq_root == sq_root.ceil();
+
+}
+
 pub fn solution() {
 
-  let mut m = 1;
+  let mut m = 1_u64;
   let mut count = 0;
 
   
@@ -10,13 +36,13 @@ pub fn solution() {
     for b in 1..a+1 {
       for c in 1..b+1 {
 
-        let a_f64 = a as f64;
-        let b_f64 = b as f64;
-        let c_f64 = c as f64;
+        let a_f64 = a as u64;
+        let b_f64 = b as u64;
+        let c_f64 = c as u64;
 
-        let len = (a_f64 * a_f64 + b_f64*b_f64 + c_f64*c_f64 + 2_f64*b_f64*c_f64).sqrt();
+        let len_sq = a_f64 * a_f64 + b_f64*b_f64 + c_f64*c_f64 + 2_u64*b_f64*c_f64;
 
-        if len == len.ceil() {
+        if is_a_square_number(len_sq) {
           count += 1;
         }
       }
@@ -28,4 +54,8 @@ pub fn solution() {
   }
 
   println!("cnt={}, M={}", count, m);
+
+  unsafe {
+    println!("is_a_square_number_entry_cnt={}, is_a_square_number_entry_cnt_opt={}", is_a_square_number_entry_cnt, is_a_square_number_entry_cnt_opt);
+  }
 }
